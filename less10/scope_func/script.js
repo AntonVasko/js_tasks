f0f964d14e4f2ca45aad5a1afdc936b88d8dd9ed
LamBust <olegoodno@gmail.com>
2025-01-23 20:11:54 +0300

    many things

--- less10/scope_func/script.js
+++ less10/scope_func/script.js
@@ -1,16 +1,30 @@
+const para = document.querySelector('ul');
+
 const x = 1;
 
 function a() {
     const y = 2;
-    
+    return y;
 }
 
 function b() {
     const z = 3;
+    return z;
 }
 
 function output(value) {
-    const para = document.createElement('p');
-    document.body.appendChild(para);
-    para.textContent = `Value: ${value}`;
+    const y = a()
+    const z = b()
+    console.log(value);
+    let listItem1 = document.createElement('li');
+    listItem1.textContent = `Value: ${value}`;
+    para.appendChild(listItem1);
+    let listItem2 = document.createElement('li');
+    listItem2.textContent = `Value: ${y}`;
+    para.appendChild(listItem2);
+    let listItem3 = document.createElement('li');
+    listItem3.textContent = `Value: ${z}`;
+    para.appendChild(listItem3);
 }
+
+output(x);
