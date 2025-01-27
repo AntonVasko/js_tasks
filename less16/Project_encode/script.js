c274838a22256170cef2ebdc88ab838ba00dfd0e
LamBust <olegoodno@gmail.com>
2025-01-27 19:53:28 +0300

    functions

---
+++ less16/Project_encode/script.js
@@ -0,0 +1,36 @@
+function encode(number, sign){
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let signed_number = '';
+    while (number != 0){
+        signed_number += numbers[number % sign];
+        number = Math.floor(number/sign);
+    }
+    signed_number = signed_number.split('').reverse().join('');
+    return signed_number;
+}
+
+function to_unsigned(number, sign){
+    ans = number%(2**(sign*8));
+    return ans;
+}
+
+function to_signed(number, sign){
+    number = to_unsigned(number, sign);
+    if (number >= 0){
+        return number;
+    } else {
+        return number - (2**(sign*8-1));
+    }
+}
+
+function to_mem(number, sign){
+    number = to_unsigned(number, sign);
+    ans = encode(number, 16);
+    ans = ans.split('').reverse().join('');
+    while (ans.length < sign*2){
+        ans += '0';
+    }
+    return ans.split('').reverse().join('');
+}
+
+//Везде number - число которое мы хотим перевести sign - количество бит в функциях 2 - 4, а функции 1 - С/И в которую хотим перевести
