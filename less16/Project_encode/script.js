653d086ba491fa364e83ba00509eba24f1ed751c
LamBust <olegoodno@gmail.com>
2025-01-27 21:20:02 +0300

    problem with document.querySelector less16/Project_encode/script.js

--- less16/Project_encode/script.js
+++ less16/Project_encode/script.js
@@ -1,3 +1,5 @@
+let table = document.querySelector('input');
+
 function encode(number, sign){
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let signed_number = '';
@@ -11,19 +13,24 @@ function encode(number, sign){
 
 function to_unsigned(number, sign){
     ans = number%(2**(sign*8));
+    if (number >= 0){
+        ans = ans
+    } else {
+        ans += 2**(sign*8)
+    }
     return ans;
 }
 
 function to_signed(number, sign){
     number = to_unsigned(number, sign);
-    if (number >= 0){
+    if (number < 2**(sign*8-1)){
         return number;
     } else {
-        return number - (2**(sign*8-1));
+        return number - (2**(sign*8));
     }
 }
 
-function to_mem(number, sign){
+function to_memory(number, sign){
     number = to_unsigned(number, sign);
     ans = encode(number, 16);
     ans = ans.split('').reverse().join('');
@@ -32,5 +39,14 @@ function to_mem(number, sign){
     }
     return ans.split('').reverse().join('');
 }
-
 //Везде number - число которое мы хотим перевести sign - количество бит в функциях 2 - 4, а функции 1 - С/И в которую хотим перевести
+
+//Оформление сайта
+
+
+for (i = 1; i <= 4; i++){
+    if (i == 3){
+        continue;
+    }
+    console.log(i);
+}
