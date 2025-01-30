304750fbf65e1486ade9b343a33159c01b92a9fa
LamBust <olegoodno@gmail.com>
2025-01-30 21:07:07 +0300

    result for decoding

--- less16/Project_encode/script.js
+++ less16/Project_encode/script.js
@@ -1,4 +1,5 @@
-let table = document.querySelector('input');
+let table = document.querySelector('table');
+let selected_number = document.querySelector('input');
 
 function encode(number, sign){
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
@@ -43,10 +44,47 @@ function to_memory(number, sign){
 
 //Оформление сайта
 
+function createTable(number){
+    let listItemPar = document.createElement('tr');
+    let listItem = document.createElement('td');
+    listItem.textContent = number;
+    listItemPar.appendChild(listItem);
+    listItem = document.createElement('td');
+    listItem.textContent = 'Memory';
+    listItemPar.appendChild(listItem);
+    listItem = document.createElement('td');
+    listItem.textContent = 'Unsigned';
+    listItemPar.appendChild(listItem);
+    listItem = document.createElement('td');
+    listItem.textContent = 'Signed';
+    listItemPar.appendChild(listItem);
+    table.appendChild(listItemPar);    
+}
 
-for (i = 1; i <= 4; i++){
-    if (i == 3){
-        continue;
+selected_number.onchange = function(){
+    table.innerHTML = '';
+    let value_number = selected_number.value;
+    selected_number.value = '';
+    createTable(value_number);
+    for (i = 1; i <= 4; i++){
+        if (i == 3){
+            continue;
+        }
+        let listItemPar = document.createElement('tr');
+        let listItem = document.createElement('td');
+        listItem.textContent = `${i} bytes`;
+        listItemPar.appendChild(listItem);
+        for (k = 0; k<3; k++){
+            listItem = document.createElement('td');
+            if (k == 0){
+                listItem.textContent = to_memory(value_number, i);
+            } else if (k==1) {
+                listItem.textContent = to_unsigned(value_number, i);
+            } else {
+                listItem.textContent = to_signed(value_number, i);
+            }
+            listItemPar.appendChild(listItem);
+        }
+        table.appendChild(listItemPar);
     }
-    console.log(i);
 }
