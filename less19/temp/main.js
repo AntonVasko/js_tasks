3a9a03270fa782fb07d6a0969d6c1a9e3c17bf60
AntonVasko <vasko.anton.a@gmail.com>
2025-02-03 16:42:14 +0300

    less19 project images

---
+++ less19/temp/main.js
@@ -0,0 +1,14 @@
+let displayedImage = document.querySelector('.displayed-img');
+let thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+let overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+  let newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+
