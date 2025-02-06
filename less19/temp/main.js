c00892312afad2d4266cbdfa24cdd2cef053911c
LamBust <olegoodno@gmail.com>
2025-02-06 20:50:00 +0300

    darker or lighter

--- less19/temp/main.js
+++ less19/temp/main.js
@@ -1,14 +1,28 @@
 let displayedImage = document.querySelector('.displayed-img');
 let thumbBar = document.querySelector('.thumb-bar');
+let allImg = document.querySelector('.full-img')
 
 btn = document.querySelector('button');
 let overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+/* Looping through images*/
 
   let newImage = document.createElement('img');
   newImage.setAttribute('src', xxx);
   thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
-
+btn.onclick = function (){
+  let btnStyle = btn.getAttribute('class');
+  let toPut = '';
+  if(btnStyle == 'dark'){
+    toPut = 'light';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    btn.textContent = 'Темнее';
+  } else {
+    toPut = 'dark';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    btn.textContent = 'Светлее';
+  }
+  btn.setAttribute('class', toPut);
+}
