f6165201078843d542ab819526f40c14a20bb233
LamBust <olegoodno@gmail.com>
2025-02-20 19:22:38 +0300

    hw with bag

--- script1.py
+++ script1.py
@@ -1,6 +1,6 @@
-s = input()
-a = s.find('h')
-z = s.rfind('h')
-new = s[a+1: z]
-print(new)
-print(s.replace(new, new.replace('h', 'H')))
+n, a, b, c, d = map(int, input().split())
+
+numbers = list(range(1, n + 1))
+numbers[a-1:b] = numbers[a-1:b][::-1]
+numbers[c-1:d] = numbers[c-1:d][::-1]
+print(*numbers)
