89db957edf1eb6fa2c684ba06f2d3bc3f177d932
LamBust <olegoodno@gmail.com>
2025-02-24 20:17:20 +0300

    lesson 22

--- script1.py
+++ script1.py
@@ -1,6 +1,8 @@
-n, a, b, c, d = map(int, input().split())
-
-numbers = list(range(1, n + 1))
-numbers[a-1:b] = numbers[a-1:b][::-1]
-numbers[c-1:d] = numbers[c-1:d][::-1]
-print(*numbers)
+n = int(input())
+a = list(map(int, input().split()))
+k = 0
+for i in range(0, len(a)):
+    for t in range(i+1, len(a)):
+        if a[i] > a[t]:
+            k += 1
+print(k)
