2370b782ed2dd29c7a62a9fa7266aaa99e2c3e32
LamBust <olegoodno@gmail.com>
2025-03-03 20:40:04 +0300

    bus hw

--- less23/hw/bus.js
+++ less23/hw/bus.js
@@ -9,26 +9,44 @@ class Transport {
     }
 }
 
+class Conductor {
+    constructor(payment) {
+        this.payment = payment;
+        this.passedPassangers = 0;
+    }
+    
+    checkPayment(pickedUpPassangers) {
+        let passangerPayment = 0
+        for (let i = 0; i < pickedUpPassangers; i++) {
+            passangerPayment = 40 + Math.floor(Math.random() * 91);
+            if (passangerPayment > this.payment){
+                console.log('Пассажир оплатил');
+                this.passedPassangers += 1;
+            } else {
+                console.log('Пассажир не оплатил');
+            }
+        }
+        return this.passedPassangers;
+    }
+}
+
 class Bus extends Transport {
     constructor(model, speed, passangers, maxPassangers) {
         super(model, speed);
         this.passangers = passangers;
         this.maxPassangers = maxPassangers;
+        this.conductor = new Conductor(65);
     }
 
     pickUpPassangers(ammountPassangers) {
-        this.passangers += this.conductor(ammountPassangers);
+        this.passangers += this.conductor.checkPayment(ammountPassangers);
+        this.conductor.passedPassangers = 0;
         if (this.passangers > this.maxPassangers) {
             console.log('Перевес!');
             this.passangers = this.maxPassangers;
         }
+        console.log(`Кондуктор проверил всех пассажиров, свободных мест осталось ${this.maxPassangers - this.passangers}`);
     }
-
-    conductor(ammountPassangers_topick) {
-        let payed =  Math.floor(Math.random() * ammountPassangers_topick);
-        console.log(`Всего заплатили ${payed} человек, а выгнали из автобуса ${ammountPassangers_topick - payed} людей.`);
-        return(payed);
-    } 
 }
 
 let bus = new Bus('Крутой автобус', 60, 0, 20);
